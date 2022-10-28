0824a9700b235fc13a452a39cca63d08d21eeb96
AmilaSamith <amilasamit@gmail.com>
2022-10-29 00:40:43 +0530

    add comments

--- src/main/java/org/example/issuer/CustomJWTTokenIssuer.java
+++ src/main/java/org/example/issuer/CustomJWTTokenIssuer.java
@@ -13,18 +13,33 @@ public class CustomJWTTokenIssuer extends JWTTokenIssuer{
         super();
     }
 
+    /**
+     * Method to customize claim values.
+     */
     @Override
     protected JWTClaimsSet createJWTClaimSet(OAuthAuthzReqMessageContext authAuthzReqMessageContext,
                                              OAuthTokenReqMessageContext tokenReqMessageContext,
                                              String consumerKey) throws IdentityOAuth2Exception {
+        // Obtain current claim set
         JWTClaimsSet jwtClaimsSet = super.createJWTClaimSet(authAuthzReqMessageContext, tokenReqMessageContext,
                 consumerKey);
+        // get new claim value (Eg: userId)
         String userId = getUserId();
+        // initialize a builder with current claims.
         JWTClaimsSet.Builder jwtClaimsSetBuilder = new JWTClaimsSet.Builder(jwtClaimsSet);
+        // change desired claim value (Eg: SUBJECT)
         jwtClaimsSetBuilder.subject(userId);
+
         return jwtClaimsSetBuilder.build();
     }
 
+    /**
+     * Method to get userID.
+     *
+     * INSTRUCTIONS:
+     * You can change the implementation to obtain userID based on username
+     * by passing username as a parameter.
+     */
     public String getUserId() {
         return  UUID.randomUUID().toString();
     }
